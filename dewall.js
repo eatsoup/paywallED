d019adeab2a0659c58441ec21ca0736cefa482fd
eatsoup <eatsoup@gmail.com>
2020-11-01 17:45:47 +0100

    new ff version, new feature

--- dewall.js
+++ dewall.js
@@ -3,11 +3,27 @@ function generateNewCookie(){
     var randomString = btoa(randomNumber);
     return randomString;
 }
+
 function getHostName(){
     var hostName = window.location.host.replace('www','')
     return hostName;
 }
+
 function setRandomCookie(){
     document.cookie = 'temptationTrackingId=' + generateNewCookie() + ';domain=' + getHostName() + ";path=/";
 }
+
+function removeCrap() {
+  var allCrap = [];
+  allCrap.push(document.getElementsByClassName("ankeiler-simple__paid-label plus-label--inline"));
+  allCrap.push(document.getElementsByClassName("plus-label is-small"));
+  allCrap.push(document.getElementsByClassName("plus-label"));
+  // allCrap.push(document.getElementsByClassName("nav__list-item"));
+  for (let crap of allCrap) {
+    for (let it of crap) {
+      it.style.visibility="hidden";
+    };
+  };
+}
 setRandomCookie();
+document.onLoad = removeCrap();
